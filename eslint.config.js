822d3d4433c792f88e26bb3d025db599675f9b57
Alexis Jorand <imperialsun@users.noreply.github.com>
2026-09-11 15:38:34 +0200

    chore: remove typescript-eslint from linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,21 +2,20 @@ import js from "@eslint/js"
 import globals from "globals"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
-import tseslint from "typescript-eslint"
 import { defineConfig, globalIgnores } from "eslint/config"
 
 export default defineConfig([
-  globalIgnores(["coverage", "dist"]),
+  globalIgnores(["coverage", "dist", "**/*.ts", "**/*.tsx"]),
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ["**/*.{js,mjs,cjs}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2024,
+      sourceType: "module",
       globals: {
         ...globals.browser,
         ...globals.node,
